87e19c1d9fe80fc77d0e6c1f9ff03d5ee7eb2f94
Abhaya <shankarabhaya9@gmail.com>
2023-09-20 01:02:05 +0530

    classes, access modifiers, methods and getter/setters in TS

--- 02-pureTS/dist/index.js
+++ 02-pureTS/dist/index.js
@@ -1,2 +1,48 @@
 "use strict";
-console.log("Started Typescript...");
+class User {
+    constructor(email, name) {
+        this.city = "";
+        this.email = email,
+            this.name = name;
+    }
+}
+// Another way of declaring class without using "this" key and also using less code
+class User2 {
+    constructor(email, name) {
+        this.email = email;
+        this.name = name;
+        this._courseCount = 1;
+        this.city = "Bengaluru";
+    }
+    // We can also create private methods
+    deleteToken() {
+        console.log("Token Deleted");
+    }
+    // Getters and setters 
+    get getCompanyEmail() {
+        const date = new Date().toLocaleTimeString();
+        console.log(date);
+        return `${this.email}${date}`;
+    }
+    get courseCount() {
+        return this._courseCount;
+    }
+    set courseCount(courseNum) {
+        if (courseNum <= 1) {
+            throw new Error("Course Count should be greater than 1");
+        }
+        this._courseCount = courseNum;
+    }
+}
+class SubUser extends User2 {
+    constructor() {
+        super(...arguments);
+        this.isFamily = true;
+    }
+    changeCourseCount() {
+        this._courseCount = 2;
+    }
+}
+const abhaya = new User2("abhaya@gmail.com", "Abhaya");
+// abhaya.city = "Dhanbad"
+// abhaya.deleteToken()
--- 02-pureTS/src/index.ts
+++ 02-pureTS/src/index.ts
@@ -1,2 +1,57 @@
-console.log("Started Typescript...");
+class User {
+    email: string
+    name: string
+    city: string = ""
+    constructor(email: string, name: string){
+        this.email = email,
+        this.name = name
+    }
+}
 
+// Another way of declaring class without using "this" key and also using less code
+class User2 {
+    protected _courseCount = 1
+    readonly city: string = "Bengaluru"
+    constructor(
+        public email: string,
+        public name: string,
+        // private userId: number  
+        ){
+    }
+
+    // We can also create private methods
+    private deleteToken() {
+        console.log("Token Deleted");
+    }
+
+    // Getters and setters 
+
+    get getCompanyEmail() : string{
+        const date = new Date().toLocaleTimeString()
+        console.log(date);
+        return `${this.email}${date}`
+        
+    }
+
+    get courseCount() : number{
+        return this._courseCount;
+    }
+
+    set courseCount(courseNum){
+        if (courseNum <= 1) {
+            throw new Error("Course Count should be greater than 1")
+        }
+        this._courseCount = courseNum
+    }
+}
+
+class SubUser extends User2{
+    isFamily: boolean = true
+    changeCourseCount(){
+        this._courseCount = 2
+    }
+}
+
+const abhaya = new User2("abhaya@gmail.com", "Abhaya")
+// abhaya.city = "Dhanbad"
+// abhaya.deleteToken()
