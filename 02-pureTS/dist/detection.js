59fba38d7bea75d1c36e892b301db94c419b8350
Abhaya <shankarabhaya9@gmail.com>
2023-09-21 20:35:50 +0530

    type predicates and discriminative unions - never in TS

--- 02-pureTS/dist/detection.js
+++ 02-pureTS/dist/detection.js
@@ -14,6 +14,7 @@ function provideID(id) {
     }
     return id.toLowerCase();
 }
+// TS Docs example:
 function printAll(strs) {
     // here we dont check if the string value is "" - empty string.
     if (strs) {
@@ -27,8 +28,50 @@ function printAll(strs) {
         }
     }
 }
+// The in operator narrowing
 function IsAdminAccount(account) {
     if ("IsAdmin" in account) {
         return account.IsAdmin;
+    }
+}
+function logValue(x) {
+    if (x instanceof Date) {
+        console.log(x.toUTCString());
+    }
+    else {
+        console.log(x.toUpperCase());
+    }
+}
+function isFish(pet) {
+    return pet.swim !== undefined;
+}
+function getFood(pet) {
+    if (isFish(pet)) {
+        pet;
+        return "Fish Food";
+    }
+    pet;
+    return "Bird Food";
+}
+function getAreaOfShape(shape) {
+    if (shape.kind === "circle") {
+        return Math.PI * shape.radius ** 2;
+    }
+    else {
+        // return shape.side ** 2;
+    }
+}
+// Suppose in future we defined another shape - Triangle, Now this _exhaustiveCheckForShape will make sure that particular case is handled or it will throw back errors. 
+function getShape(shape) {
+    switch (shape.kind) {
+        case "circle":
+            return Math.PI * shape.radius ** 2;
+        case "square":
+            return shape.side ** 2;
+        case "rectangle":
+            return shape.length * shape.breadth;
+        default:
+            const _exhaustiveCheckForShape = shape;
+            return _exhaustiveCheckForShape;
     }
 }
--- 02-pureTS/src/detection.ts
+++ 02-pureTS/src/detection.ts
@@ -15,6 +15,7 @@ function provideID(id: string | null) {
     return id.toLowerCase()
 }
 
+// TS Docs example:
 function printAll(strs: string | string[] | null) {
     // here we dont check if the string value is "" - empty string.
     if (strs) {
@@ -39,9 +40,80 @@ function printAll(strs: string | string[] | null) {
     isAdmin: boolean
   }
 
+  // The in operator narrowing
   function IsAdminAccount(account: User | Admin){
     if("IsAdmin" in account){
         return account.IsAdmin
     }
   }
 
+  function logValue(x: Date | string) {
+    if (x instanceof Date) {
+      console.log(x.toUTCString());
+    } else {
+      console.log(x.toUpperCase());
+    }
+  }
+
+  // using Type predicates 
+
+  type Fish = {swim() : void}
+  type Bird = {fly() : void}
+
+  function isFish(pet: Fish | Bird): pet is Fish {
+    return (pet as Fish).swim !== undefined;
+  }
+
+  function getFood(pet: Fish | Bird){
+    if (isFish(pet)) {
+      pet
+      return "Fish Food"
+    }
+    pet
+    return "Bird Food"
+  }
+
+// Discriminative Unions
+
+interface Circle {
+  kind: "circle",
+  radius: number
+}
+
+interface Square {
+  kind: "square",
+  side: number
+}
+
+interface Rectangle {
+  kind: "rectangle",
+  length: number,
+  breadth: number
+}
+
+type Shape = Circle | Square | Rectangle
+
+function getAreaOfShape(shape: Shape){
+  if (shape.kind === "circle") {
+    return Math.PI * shape.radius ** 2;
+  }
+  else{
+    // return shape.side ** 2;
+  }
+}
+
+// Suppose in future we defined another shape - Triangle, Now this _exhaustiveCheckForShape will make sure that particular case is handled or it will throw back errors. 
+
+function getShape(shape: Shape){
+  switch(shape.kind){
+    case "circle":
+      return Math.PI * shape.radius ** 2;
+    case "square":
+      return shape.side ** 2;
+    case "rectangle":
+      return shape.length * shape.breadth; 
+    default:
+      const _exhaustiveCheckForShape : never = shape
+      return _exhaustiveCheckForShape 
+  }
+}
